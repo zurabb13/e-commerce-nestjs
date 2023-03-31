a99be78fcfaa536cfa7ccac89acfbcb2970c73e1
zura <zura.baramia98@outook.com>
2023-03-31 13:17:37 +0400

    feat( ) initial nestjs app Closes #1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(process.env.MONGO),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
