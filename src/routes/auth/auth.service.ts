4c8bb0ebf65c8fec39c5ed807845cbfa8f69491a
zura <zura.baramia98@outook.com>
2023-03-31 19:20:44 +0400

    auth service & module update

--- src/routes/auth/auth.service.ts
+++ src/routes/auth/auth.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(username: string, password: string): Promise<any> {
     const user = await this.userService.find(username);
@@ -12,5 +17,15 @@ export class AuthService {
       return user;
     }
     return null;
+  }
+  async login(user: any) {
+    const payload = {
+      username: user.username,
+      sub: user._id,
+      roles: user.roles,
+    };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
