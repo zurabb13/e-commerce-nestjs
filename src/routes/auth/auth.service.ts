8ca21bbedb470192a0aa67f85c0deda92248e1b7
zura <zura.baramia98@outook.com>
2023-03-31 19:13:49 +0400

    strategy

--- src/routes/auth/auth.service.ts
+++ src/routes/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
-
+import { UserService } from '../user/user.service';
+import * as bcrypt from 'bcrypt';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.userService.find(username);
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (user && passwordMatch) {
+      return user;
+    }
+    return null;
+  }
+}
