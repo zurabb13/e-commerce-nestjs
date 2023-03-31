30131eecbed19abb8a3f801fe029119e576eb5ea
zura <zura.baramia98@outook.com>
2023-03-31 19:52:01 +0400

    feat( ) initial nestjs app role guards and Cotroller Closes #9

--- src/routes/auth/auth.controller.ts
+++ src/routes/auth/auth.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UserService } from '../user/user.service';
+import { UserDto } from '../../dto/user.dto';
+import { LocalAuthGuard } from '../../guard/local.auth.guard';
+import { RolesGuard } from '../../guard/roles.guard';
+import { Roles } from './role.decorator';
+import { Role } from '../../enum/role.enum';
+import { JwtAuthGuard } from '../../guard/jwt.auth.guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+  ) {}
+
+  @Post('register')
+  async register(@Body() creatUser: UserDto) {
+    const user = await this.userService.create(creatUser);
+    return user;
+  }
+
+  @UseGuards(LocalAuthGuard, RolesGuard)
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.User)
+  @Get('user')
+  getUser(@Request() req) {
+    return req.user;
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Admin)
+  @Get('admin')
+  getAdmin(@Request() req) {
+    return req.user;
+  }
+}
